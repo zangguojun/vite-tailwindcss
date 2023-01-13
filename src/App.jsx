fb1b7723a060475f0f15df81af62c16c76870fcb
tong <lbuchiyu@gmail.com>
2023-01-13 23:08:18 +0800

    init: init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+
+function App() {
+
+  return (
+    <div className="App">
+      <img src={reactLogo} className="logo react" alt="React logo" />
+      <button className="btn">Hello daisyUI</button>
+    </div>
+  )
+}
+
+export default App
